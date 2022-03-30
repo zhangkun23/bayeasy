754fd8b3ff5463c07702c3f1dc47067c1ecd567a
Jingyu He <gtouchgogo@163.com>
2022-03-30 20:29:00 +0800

    button component

---
+++ miniprogram/components/login-btn/login-btn.ts
@@ -0,0 +1,41 @@
+// components/login-btn/login-btn.ts
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    text: String,
+    fontSize: {
+      type: String,
+      value: '34rpx;'
+    },
+    btnWidth: {
+      type: String,
+      value: '20rpx'
+    },
+    btnHeight: {
+      type: String,
+      value: '20rpx'
+    },
+    btnBgColor: {
+      type: String,
+      value: 'linear-gradient(94.38deg, #0080FF 0%, #2256FF 99.56%)'
+    },
+  },
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onClick(){
+      console.log("click btn")
+      this.triggerEvent('click', null, {})
+    }
+  }
+})
