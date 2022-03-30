3dc5d7cfaeb0bf8754582e042069fd49ccff76ab
Jingyu He <gtouchgogo@163.com>
2022-03-30 20:28:12 +0800

    personal page ui

---
+++ miniprogram/pages/personal/components/gates-btn/gates-btn.js
@@ -0,0 +1,49 @@
+// pages/personal/components/gates-btn/gates-btn.ts
+Component({
+  options: {
+    styleIsolation: 'isolated'
+  },
+  lifetimes: {
+    show: function() {
+      console.log("properties" ,this.properties)
+    },
+    attached: function() {
+      console.log("properties" ,this.properties)
+
+    }
+
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: String,
+    iconUrl: String,
+    gateWidth: {
+      type: String,
+      value: '20rpx'
+    },
+    gateHeight: {
+      type: String,
+      value: '20rpx'
+    },
+    fontSize: {
+      type: String,
+      value: '24rpx'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
