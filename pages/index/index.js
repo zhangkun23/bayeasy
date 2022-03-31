6cb076b876df2cd4f3937af4646c901ffb112c27
babymm1122 <xwl281928290@163.com>
2022-03-31 20:09:00 +0800

    asd

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,24 +5,36 @@ const {
   login,
   logout
 } = require('../../http/api/api.js')
+const tempPath = getApp().globalData.imgPath;
 Component({
   data: {
-    imgpath: getApp().globalData.imgPath + 'footer/null.png',
-    banner: getApp().globalData.imgPath + 'index/banner.png',
-    logo_byz: getApp().globalData.imgPath + 'public/logo_byz.png',
-    cbfy: getApp().globalData.imgPath + 'index/cbfy.png',
-    faq: getApp().globalData.imgPath + 'index/faq.png',
-    nssb: getApp().globalData.imgPath + 'index/nssb.png',
-    srzdfp: getApp().globalData.imgPath + 'index/srzdfp.png',
-    zcjc: getApp().globalData.imgPath + 'index/zcjc.png',
+    imgpath:tempPath + 'footer/null.png',
+    banner: tempPath + 'index/banner.png',
+    logo_byz: tempPath + 'public/logo_byz.png',
+    cbfy: tempPath + 'index/cbfy.png',
+    faq: tempPath + 'index/faq.png',
+    nssb: tempPath + 'index/nssb.png',
+    srzdfp: tempPath + 'index/srzdfp.png',
+    zcjc: tempPath + 'index/zcjc.png',
+    fwjs: tempPath + 'index/fwjs.png',
+    daiban: tempPath +'index/daiban.png',
+    dbNum:'11', //待办数量
+    daibanShow:true,
   },
-
 
   methods: {
     handelClick() {
       wx.navigateTo({
         url: '../login/authentication/index',
       })
+    },
+    handelClickLogin(){
+      wx.navigateTo({
+        url: '../login/login/index',
+      })
+    },
+    handelClickQqr(){
+
     }
   },
 
