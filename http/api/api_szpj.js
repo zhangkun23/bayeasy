37f8439323d26498bb15230a388bea1bba85909c
babymm1122 <xwl281928290@163.com>
2022-04-11 19:03:19 +0800

    手动上传发票

--- http/api/api_szpj.js
+++ http/api/api_szpj.js
@@ -1,11 +1,18 @@
 //企业信息
 const { request } = require('../request.js')
+const api = '';
 //基于业务封装的接口
 module.exports={
+    /*查询收货地址*/
+    getUserMeg:()=>{
+        return request(api + '/personal_nformation/mail_address','GET',{});
+    },
 
-	
+    //手动踢啊家增值发票
+    updateHandlInvoice:(param)=>{
+		return request(api+'/auth/deduct_invoice/submit_deduct_invoice','POST',param);
+	},
 
-	
-	
+
 }
 
