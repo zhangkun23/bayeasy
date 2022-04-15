ba96c0fe6b609a8e6dc4420a8303cea55fc64284
zhangkun23 <lala0098@163.com>
2022-04-15 16:33:53 +0800

    纳税申报确认开发结束

--- pages/tax/deatil/deatil.js
+++ pages/tax/deatil/deatil.js
@@ -1,7 +1,8 @@
 // pages/tax/deatil/deatil.js
 const tempPath = getApp().globalData.imgPath;
 const {
-  declareInfo
+  declareInfo,
+  confirmdeclare
 } = require('../../../http/api/api_csbl');
 Page({
 
@@ -17,6 +18,12 @@ Page({
     taxList: [], // 明细列表
     isShowModal: false,
     showBtn: false,
+    didClick: true,
+    time: '',
+    backgroundColor: '#E5EEF7',
+    boxShadow: '0rpx 0rpx 0rpx 0rpx rgba(255, 255, 255, 1)',
+    splicingStr: '(5s)',
+    timestamp: '',
     buttons: [{
         text: '取消'
       },
@@ -42,10 +49,14 @@ Page({
 
   //overdue_status 0 逾期 1 未逾期
   confirmTax() {
-    // console.log(2131)
-    this.setData({
-      isShowModal: true
-    })
+    if (this.data.didClick) {
+      this.setData({
+        isShowModal: true
+      })
+    } else {
+      return
+    }
+
   },
   tapDialogButton(e) {
     // console.log(e)
@@ -54,37 +65,52 @@ Page({
         isShowModal: false
       })
     } else {
-      wx.navigateTo({
-        url: '../successfully/index',
-      })
+      let params = {
+        id: wx.getStorageSync('detailId')
+      }
+      console.log(this.data.time)
+      if (this.data.time == 1) {
+        confirmdeclare(params).then(res => {
+          console.log(res)
+          if (res.ret) {
+            wx.navigateTo({
+              url: '../successfully/index',
+            })
+          }
+        })
+
+      }
     }
   },
 
+  showToast() {
+    this.setData({
+      showTips: true
+    })
+  },
   // 获取详情
   getdeclareInfo() {
     declareInfo(this.data.detailId).then(res => {
       console.log(res, '详情')
       if (res.ret) {
-        let time = res.data.overdue_time
-        let time1 = this.getDuration(time)
         let arr = []
-        if (res.data)
+        if (res.data) {
+          var time = res.data.overdue_time;
+          this.countDown(time)
           if (res.data.list.length > 0) {
             arr = res.data.list[0].list
           } else {
             arr = res.data.list
           }
-        // console.log(arr)
-        this.setData({
-          deatilObj: res.data,
-          timeOut: time1,
-          taxList: arr
-        })
-        // console.log(this.data.timeOut)
+          this.setData({
+            deatilObj: res.data,
+            taxList: arr
+          })
+        }
       }
     })
   },
-
+  // 秒转换为天时分秒
   getDuration(second) {
     var days = Math.floor(second / 86400);
     var hours = Math.floor((second % 86400) / 3600);
@@ -98,19 +124,73 @@ Page({
     }
     return duration;
   },
+  // 确认税金倒计时
+  renderSecon() {
+    let time = 5;
+    let str = ''
+    let timer = setInterval(() => {
+      time--;
+      if (time > 0) {
+        str = '(' + time + 's)'
+        this.setData({
+          time: time,
+          splicingStr: str,
+          didClick: false,
+        })
+      } else {
+        this.setData({
+          splicingStr: '',
+          didClick: true,
+          backgroundColor: '#1D83F0;',
+          boxShadow: '0rpx 30rpx 50rpx 0rpx rgba(29, 131, 240, 0.2);'
+        })
+      }
+      if (time <= 0) {
+        clearInterval(timer)
+      }
+    }, 1000);
+  },
+
+  /**
+   * 倒计时
+   * @param {*} type 类型 0-给定具体多少秒倒计时（默认）；1-给定目标结束时间戳，单位为秒，目标是转换为“xx秒后结束”的格式
+   * @param {*} timestamp 当type==0时表示具体秒数，默认60；type==1时为目标结束时间戳，目标是转换为“xx天xx小时xx分xx秒”的格式
+   */
+  countDown(timestamp, type) {
+    let seconds = timestamp // 倒计时总秒数
+    if (type == 1) {
+      let currentTimestamp = Math.round(new Date() / 1000) // 当前时间戳，单位秒
+      seconds = timestamp - currentTimestamp
+    }
+    // 如果目标时间小于等于当前时间，不需要继续进行了
+    if (seconds <= 0) return
+    // 定时器
+    let timer = setInterval(() => {
+      seconds--
+      let result = type == 1 ? seconds : this.getDuration(seconds)
+      this.setData({
+        timeOut: result
+      })
+      if (seconds <= 0) {
+        clearInterval(timer)
+        console.log('倒计时结束，清除定时器，避免内存溢出')
+      }
+    }, 1000)
+  },
+
   methods: {},
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.renderSecon();
+    this.countDown(this.data.timestamp);
     let id = undefined;
     if (options.id) {
-      console.log(132)
       id = options.id;
       wx.setStorageSync('detailId', options.id);
     } else {
-      console.log(987)
       id = wx.getStorageSync('detailId');
       this.setData({
         showBtn: true
