2b8051271230bf8c778237f08d9ce769ca313eb7
zhangkun23 <lala0098@163.com>
2022-04-22 15:11:22 +0800

    优化

--- pages/tax/successfully/index.js
+++ pages/tax/successfully/index.js
@@ -13,8 +13,11 @@ Page({
     tax: 800,
     paramsId: 0
   },
+  // 返回主页
   backTaxIndex() {
-   
+    wx.navigateTo({
+      url: '../taxreturn/index',
+    })
   },
   gotoReult() {
     // wx.navigateTo({
@@ -22,14 +25,17 @@ Page({
     // })
     // wx.navigateBack()
     wx.navigateBack({
-      delta:1
+      delta: 1
     })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    if(options.id) {
+    this.setData({
+      tax: options.taxPayable
+    })
+    if (options.id) {
       this.setData({
         paramsId: options.id
       })
