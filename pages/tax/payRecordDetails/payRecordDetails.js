efcb855e5cc84b5c545c10309e43281bda480f25
Jingyu He <gtouchgogo@163.com>
2022-05-11 19:02:07 +0800

    改高度

--- pages/tax/payRecordDetails/payRecordDetails.js
+++ pages/tax/payRecordDetails/payRecordDetails.js
@@ -139,12 +139,6 @@ Page({
     this.renderPage(options.type)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
 
   /**
    * 生命周期函数--监听页面显示
@@ -153,39 +147,9 @@ Page({
     this.getdeclareInfo();
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  bindPickerChange: function(e){
+      console.log(e.detail.value)
+      this.updateList()
+      this.updateItemInfo()
   }
 })
