eb7995b002821f5fe3f4aa409c1a0f6ae7a8cfdf
zhangkun23 <lala0098@163.com>
2022-06-14 17:21:46 +0800

    新建联系客服页面

--- http/api/api_grzx.js
+++ http/api/api_grzx.js
@@ -1,30 +1,33 @@
 //财税办理
-const { request } = require('../request.js')
+const {
+	request
+} = require('../request.js')
 const api = '';
 //基于业务封装的接口
-module.exports={
+module.exports = {
 
 	/**
 	 * 登录
 	 * @param { mobile:'18513136572',captcha:'111111'}  
 	 */
-	check_operate:(param)=>{
-		return request(api+'/personal_nformation/my_operate','GET',{},true);
+	check_operate: (param) => {
+		return request(api + '/personal_nformation/my_operate', 'GET', {}, true);
 	},
 
-	/* 获取二维码 */
+
 	// get_operate:()=>{
 	// 	return request('/gshApi/personal_nformation/operate_wx','GET',{},'arrayBuffer');
 	// },
-	get_operate:()=>{
-		return request(api+'/personal_nformation/operate_wx_qrcode','GET',{});
+
+	/* 获取二维码 */
+	get_operate: () => {
+		return request(api + '/personal_nformation/operate_wx_qrcode', 'GET', {});
 	},
 
 	/* 获取用户姓名 */
-	get_user_info: ()=>{
-		return request('/personal_nformation/get','GET', {} )
+	get_user_info: () => {
+		return request('/personal_nformation/get', 'GET', {})
 	}
-	
-	
-}
 
+
+}
