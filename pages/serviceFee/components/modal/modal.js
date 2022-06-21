fab69e1ece19b7583cb92127532c17dcd2ed5b3e
zhangkun23 <lala0098@163.com>
2022-06-21 11:51:44 +0800

    解开注释

--- pages/serviceFee/components/modal/modal.js
+++ pages/serviceFee/components/modal/modal.js
@@ -61,7 +61,7 @@ Component({
       value: "",
     }
   },
-  
+
   /**
    * 组件的初始数据
    */
@@ -75,72 +75,72 @@ Component({
   methods: {
     // 确认支付
     topay() {
-      // if (this.data.title == '支付完成') {
-      //   this.setData({
-      //     showModal: false
-      //   })
-      // } else {
-      //   let open_id = getApp().globalData.open_id;
-      //   let app_id = getApp().globalData.app_id;
-      //   if (!open_id) return;
-      //   提交预订单
-      //   let params = {
-      //     open_id, // 用户id,后端返回
-      //     order_no: this.data.orderno, // 订单号
-      //     amount: this.data.money, // 金额
-      //     app_id
-      //   }
-      //   提交预支付订单
-      //   wechatPay(params).then(res => {
-      //     if (res.ret) {
-      //       let paymentarams = {
-      //         timeStamp: res.data.timeStamp,
-      //         nonceStr: res.data.nonceStr,
-      //         package: res.data.package,
-      //         signType: res.data.signType,
-      //         paySign: res.data.paySign,
-      //       }
-      //       调起微信支付控件
-      //       wx.requestPayment({
-      //         "timeStamp": paymentarams.timeStamp + '',
-      //         "nonceStr": paymentarams.nonceStr,
-      //         "package": paymentarams.package,
-      //         "signType": paymentarams.signType,
-      //         "paySign": paymentarams.paySign,
-      //         "success": function (res) {
-      //           this.setData({
-      //             title: '支付完成',
-      //             btnText: '完成',
-      //             showModal: false
-      //           })
-      //           let data = {
-      //             title: this.data.title,
-      //             btnText: this.data.btnText,
-      //           }
-      //           this.triggerEvent("sendParent", data)
+      if (this.data.title == '支付完成') {
+        this.setData({
+          showModal: false
+        })
+      } else {
+        let open_id = getApp().globalData.open_id;
+        let app_id = getApp().globalData.app_id;
+        if (!open_id) return;
+        提交预订单
+        let params = {
+          open_id, // 用户id,后端返回
+          order_no: this.data.orderno, // 订单号
+          amount: this.data.money, // 金额
+          app_id
+        }
+        提交预支付订单
+        wechatPay(params).then(res => {
+          if (res.ret) {
+            let paymentarams = {
+              timeStamp: res.data.timeStamp,
+              nonceStr: res.data.nonceStr,
+              package: res.data.package,
+              signType: res.data.signType,
+              paySign: res.data.paySign,
+            }
+            调起微信支付控件
+            wx.requestPayment({
+              "timeStamp": paymentarams.timeStamp + '',
+              "nonceStr": paymentarams.nonceStr,
+              "package": paymentarams.package,
+              "signType": paymentarams.signType,
+              "paySign": paymentarams.paySign,
+              "success": function (res) {
+                this.setData({
+                  title: '支付完成',
+                  btnText: '完成',
+                  showModal: false
+                })
+                let data = {
+                  title: this.data.title,
+                  btnText: this.data.btnText,
+                }
+                this.triggerEvent("sendParent", data)
 
-      //           this.triggerEvent('closeBtn')
-      //           wx.navigateTo({
-      //             url: '/pages/serviceFee/paymentSuccessful/index?starttime=' + this.data.starttime + '&endtime=' + this.data.endtime + '&money=' + this.data.money + '&orderno=' + this.data.orderno,
-      //           })
-      //         },
-      //         "fail": function (res) {
+                this.triggerEvent('closeBtn')
+                wx.navigateTo({
+                  url: '/pages/serviceFee/paymentSuccessful/index?starttime=' + this.data.starttime + '&endtime=' + this.data.endtime + '&money=' + this.data.money + '&orderno=' + this.data.orderno,
+                })
+              },
+              "fail": function (res) {
                 wx.navigateTo({
                   url: '/pages/serviceFee/paymentError/index?starttime=' + this.data.starttime + '&endtime=' + this.data.endtime + '&money=' + this.data.money,
                 })
-      //         },
-      //         "complete": function (res) {
-      //           console.log(res)
-      //         }
-      //       })
-      //     } else {
-      //       wx.showToast({
-      //         title: res.message,
-      //         icon: "none"
-      //       })
-      //     }
-      //   })
-      // }
+              },
+              "complete": function (res) {
+                console.log(res)
+              }
+            })
+          } else {
+            wx.showToast({
+              title: res.message,
+              icon: "none"
+            })
+          }
+        })
+      }
     },
 
     // 关闭模态框
