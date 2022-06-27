32736dd925cf2c08bca24ed2f44d7f999424a879
zhangkun23 <lala0098@163.com>
2022-06-27 10:43:38 +0800

    个人中心添加我的邮箱

--- pages/personal/myEmail/index.js
+++ pages/personal/myEmail/index.js
@@ -1,8 +1,6 @@
 // pages/personal/myEmail/index.js
 const tempPath = getApp().globalData.imgPath;
-const {
-  resetEmail
-} = require('../../../http/api/api')
+
 Page({
   /**
    * 页面的初始数据
@@ -16,24 +14,14 @@ Page({
     isValid: false,
     btnBackgroundColor: "#E6EEF7",
     btnColor: "#ABBED2",
+    globalEmail: getApp().globalData.email,
     isShowUpdateEmail: false,
-    disabled: false,
   },
   onInput(e) {
     let value = e.detail.value;
     if (value) {
       this.setData({
         color: "#1D83F0",
-        btnBackgroundColor: "#1D83F0",
-        btnColor: "#fff"
-      });
-    } else {
-      this.setData({
-        inputValue: "",
-        isValid: false,
-        color: "#E6EEF7",
-        btnBackgroundColor: "#E6EEF7",
-        btnColor: "#ABBED2",
       });
     }
     this.setData({
@@ -49,7 +37,6 @@ Page({
     });
   },
   onBlur(e) {
-    console.log(e)
     let value = e.detail.value;
     if (!value) {
       this.setData({
@@ -57,10 +44,6 @@ Page({
         isValid: false,
       });
     } else {
-      this.setData({
-        isShowUpdateEmail: false,
-        resStart: e.detail.cursor
-      })
       this.verifyEmail(value);
     }
     this.setData({
@@ -95,110 +78,53 @@ Page({
       });
     }
   },
-  // 修改邮箱
-  updateEmail(e) {
-    this.setData({
-      disabled: false,
-      color: '#1D83F0',
-      isShowUpdateEmail: false,
-      btnBackgroundColor: "#1D83F0",
-      btnColor: "#fff",
-      isShowIcon: true
-    })
-  },
-  // 保存邮箱
-  sureEmail() {
+
+  updateEmail() {
     let val = this.data.inputValue;
-    let reg = new RegExp(
-      "^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(.[a-zA-Z0-9-]+)*.[a-zA-Z0-9]{2,6}$"
-    );
-    if (reg.test(val)) {
-      resetEmail({
-        email: val
-      }).then(res => {
-        if (res.ret) {
-          getApp().globalData.email = res.data.email;
-          this.setData({
-            isShowUpdateEmail: true,
-            disabled: true,
-            color: "#E6EEF7",
-          })
-        } else {
-          wx.showToast({
-            title: res.message,
-            icon: "none"
-          })
-        }
-      })
-    } else {
-      this.setData({
-        isValid: true,
-        color: "#FF475A",
-      });
-    }
-  },
-
-  // 初始化邮箱
-  initVal() {
-    let globalEmail = getApp().globalData.email;
-    if (globalEmail) {
-      this.setData({
-        inputValue: globalEmail,
-        color: "#E6EEF7",
-        isShowUpdateEmail: true,
-        disabled: true
-      })
-    } else {
-      this.setData({
-        inputValue: "",
-        isValid: false,
-        color: "#E6EEF7",
-        btnBackgroundColor: "#E6EEF7",
-        btnColor: "#ABBED2",
-
-      })
-    }
+    update(val).then((res) => {
+      if (res.ret) {
+        this.setData({});
+      }
+    });
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    this.initVal()
-  },
+  onLoad: function(options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {},
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {},
+  onShow: function() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {},
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {},
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {},
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {},
+  onReachBottom: function() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {},
+  onShareAppMessage: function() {},
 });
