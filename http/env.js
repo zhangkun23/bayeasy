bb7a1bd6628039cf6b9f270dcdb31352ff245741
zhangkun23 <lala0098@163.com>
2022-07-07 14:39:28 +0800

    更换pdf体验版地址

--- http/env.js
+++ http/env.js
@@ -1,37 +1,40 @@
-    const version = __wxConfig.envVersion;
-    let baseUrl;
-    let payUrl;
-    let pdfUrl = "https://cs.bayeasy.cn/betaUpload/"
-    switch (version) {
-        case "develop": //开发预览版
-            baseUrl = "https://cs.bayeasy.cn/betaApi";
-            payUrl = "https://cs.bayeasy.cn/";
-            break;
-        case 'trial': //体验版
-            baseUrl = "https://cs.bayeasy.cn/betaApi";
-            break;
-        case 'release': //正式版
-            baseUrl = "https://cs.bayeasy.cn/api";
-            pdfUrl = "https://cs.bayeasy.cn"
-            break;
-        default: //未知,默认调用正式版
-            break;
-    }
+const version = __wxConfig.envVersion;
+let baseUrl;
+let payUrl;
+let pdfUrl;
+switch (version) {
+  case "develop": //开发预览版
+    baseUrl = "https://cs.bayeasy.cn/betaApi";
+    payUrl = "https://cs.bayeasy.cn/";
+    pdfUrl = "https://cs.bayeasy.cn/betaUpload/";
+    break;
+  case "trial": //体验版
+    baseUrl = "https://cs.bayeasy.cn/grayApi";
+    pdfUrl = "https://cs.bayeasy.cn/grayUpload/";
+    break;
+  case "release": //正式版
+    baseUrl = "https://cs.bayeasy.cn/api";
+    pdfUrl = "https://cs.bayeasy.cn/upload/";
+    break;
+  default:
+    //未知,默认调用正式版
+    break;
+}
 
-    //这里使用的接口呢都是自己模拟的，可以根据自己的需求进行添加
-    module.exports = {
-        pdfUrl,
-        //开发环境的url
-        dev: {
-            baseUrl,
-            payUrl
-        },
-        //测试环境url
-        test: {
-            baseUrl
-        },
-        //线上环境url
-        prod: {
-            baseUrl: 'https://cs.bayeasy.cn/api'
-        }
-    }
+//这里使用的接口呢都是自己模拟的，可以根据自己的需求进行添加
+module.exports = {
+  pdfUrl,
+  //开发环境的url
+  dev: {
+    baseUrl,
+    payUrl,
+  },
+  //测试环境url
+  test: {
+    baseUrl,
+  },
+  //线上环境url
+  prod: {
+    baseUrl: "https://cs.bayeasy.cn/api",
+  },
+};
